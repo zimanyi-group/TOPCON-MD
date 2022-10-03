5357339ad376770ba8666dc0feec727d326c109f
Adam Goga <adfgoga@gmail.com>
2022-10-03 07:15:05 -0700

    weekend changes

--- farm-run-lammps.sh
+++ farm-run-lammps.sh
@@ -10,41 +10,32 @@
 #SBATCH --mail-type=FAIL,END
 
 
-
-#======
 #SBATCH --ntasks=128
 #SBATCH --ntasks-per-node=128
 #SBATCH --cpus-per-task=1 
 #SBATCH --mem=64G
 #SBATCH -t 4-00:00
 
-###COMMAND LINE ARGUMENTS
-###1ST FILE NAME
-###2ND 'farm' or no to tell if farm or not
-###3RD 
 
 lmppre='lmp/'
-FILE=$1
-FILENAME=${1#"$lmppre"} #"SilicaAnneal.lmp"
+FILE=$1 #first input should be like "lmp/SilicaAnneal.lmp"
+FILENAME=${1#"$lmppre"} 
 
 j=$SLURM_JOB_ID
 
 NAME=${FILENAME%.*}
 
-if [[ $2 == "farm" ]]; then
-    UNIQUE_TAG="-FARM-"$SLURM_JOBID
-else 
-    UNIQUE_TAG=$(date +%m%d-%Hh%Mm%S)
-fi
-
+UNIQUE_TAG="-FARM-"$SLURM_JOBID
 
 CWD=$(pwd) #current working directory
 OUT_FOLDER=$CWD"/output/"${NAME}${UNIQUE_TAG}"/"
+
 
 mkdir -p $CWD"/output/" #just in case output folder is not made
 mkdir $OUT_FOLDER #Now make folder where all the output will go
 
 
+#copy the file to run into the new directory
 IN_FILE=$CWD"/"$FILE
 LOG_FILE=$OUT_FOLDER$NAME".log"
 cp $IN_FILE $OUT_FOLDER
@@ -53,15 +44,19 @@ s=$OUT_FOLDER$NAME"_SLURM.txt"
 
 
 
-
-
-
 export OMP_NUM_THREADS=1
 # export OMP_PLACES=threads
 # export OMP_PROC_BIND=true
 
-if [[ $2 == "farm" ]]; then    #                                                          Creates a variable in lammps ${output_folder}
-    srun /home/agoga/sandbox/lammps/lmp_mpi -nocite -log $LOG_FILE -in $OUT_FOLDER$FILENAME -var output_folder $OUT_FOLDER
+#                                                          Creates a variable in lammps ${output_folder}
+if srun /home/agoga/sandbox/lammps/lmp_mpi -nocite -log $LOG_FILE -in $OUT_FOLDER$FILENAME -var output_folder $OUT_FOLDER ; 
+then #rename the output directory to show it's finished
+    #srun completed succesfully 
+    mv $OUT_FOLDER ${OUT_FOLDER%?}"-S"
 else
-    mpirun -np 2 lmp_mpi -nocite -log $LOG_FILE -in $OUT_FOLDER$FILENAME -var output_folder $OUT_FOLDER
+    #srun failed somehow
+    mv $OUT_FOLDER ${OUT_FOLDER%?}"-F"
+    exit 1
 fi
+
+
