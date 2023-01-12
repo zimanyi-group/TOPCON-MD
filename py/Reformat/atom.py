427519417c5d69adfad339837354511fb4910cda
Adam Goga <adfgoga@gmail.com>
2023-01-12 10:07:26 -0800

    Structure analysis and some peturb fixes

---
+++ py/Reformat/atom.py
@@ -0,0 +1,155 @@
+import numpy as np
+
+
+#==============
+#boundary wrapper 
+# only use if atom.x + cuttoff goes through the simbox
+#cutoff=2.8 #angstrom
+# when we are looking for neighbors we will measure the distance from atom-a to atom-b
+# if the distance is greater than 1/2 the sim box use wrapped coords
+#step 1 measure distance()
+def Compute_Vec(a1, a2):
+    vec=np.empty([3],dtype = float)
+    if(a1.id == a2.id):
+        x1, x2 = a1.coords, a1.initial
+    else:
+        x1, x2 = a1.coords, a2.coords
+    dist= 0
+    for i in range(3):
+        box_length=ATOM.box[i][1] - ATOM.box[i][0]
+        delta=abs(x1[i] - x2[i])
+        if(delta > 0.5*box_length):
+            delta = least_dist([x1[i],x2[i]],box_length)
+        vec[i] = delta
+    return vec
+
+
+def distance(a1,a2):
+    if(a1.id == a2.id):
+        x1, x2 = a1.coords, a1.initial
+    else:
+        x1, x2 = a1.coords, a2.coords
+    dist= 0
+    for i in range(3):
+        box_length=ATOM.box[i][1] - ATOM.box[i][0]
+        delta=abs(x1[i] - x2[i])
+        if(delta > 0.5*box_length):
+            delta = least_dist([x1[i],x2[i]],box_length)
+        dsquare = delta**2
+        dist+=dsquare
+    return np.sqrt(dist)
+    
+def least_dist(s,bl):
+    x1, x2 = s
+    if(x1>x2):
+        return abs((x2 + bl) - x1)
+    else:
+        return abs((x1 + bl) - x2)
+
+def boundary_wrapper(ar):
+    new_coords=np.empty([3])
+    for i in [0,1,2]:
+        lb, ub =ATOM.box[i]
+        if(ar[i] < lb):
+            new_coords[i] = ar[i] + ub
+        elif(ar[i] > ub):
+            new_coords[i] = ar[i] - ub
+        else:
+            new_coords[i] = ar[i]
+    return new_coords
+
+def Compute_Volume(cutoff):
+    box_length = []
+    x = ATOM.box[0][1] - ATOM.box[0][0]
+    y = ATOM.box[1][1] - ATOM.box[1][0]
+    z = ATOM.box[2][1] - cutoff
+    # there are 10^7 A in one cm => 1 = 1cm/10^7A
+    return (x*y*z)*1.0e-24
+#===========================================
+class ATOM():
+    cutoff=2.8
+    box=None
+    NUM = None
+    
+    def __init__(self, vals):
+        self.id = vals[0]
+        self.type = vals[1]
+        self.x = vals[2]
+        self.y = vals[3]
+        self.z = vals[4]
+        self.neighbors = []
+        self.initial=np.array([vals[2], vals[3], vals[4]])
+    
+    @property
+    def coords(self):
+        return np.array([self.x, self.y, self.z])
+    
+    @coords.setter
+    def coords(self, ar):
+        self.x, self.y, self.z = boundary_wrapper(ar)
+
+    def scaled_coords(self):
+        s=np.diff(ATOM.box,axis=1).reshape(3)
+        return np.divide(self.coords,s)
+
+    def get_neighs(self,array_of_atoms):
+        for i in range(self.id,ATOM.NUM):
+            at = array_of_atoms[i]
+            if(distance(self,at) <= ATOM.cutoff):
+                self.neighbors.append(at)
+                at.neighbors.append(self)
+        self.neighbors=np.asarray(self.neighbors)
+
+    def reset(self):
+        self.coords = self.initial
+    
+    def distance_moved(self):
+        atype=['oops!', 'H', 'Si']
+        d=distance(self,self)
+        txt="{0} atom {1} moved {2:.3f}".format(atype[self.type], self.id, d)
+        print(txt)
+        return d
+    
+    @classmethod
+    def set_box(cls,sim_box):
+        cls.box=sim_box
+        return
+
+    @classmethod
+    def set_num(cls,num):
+        cls.NUM=num
+        return
+#======================================================================
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
--- py/analysis.py
+++ py/analysis.py
@@ -1,79 +1,119 @@
 from ovito.io import import_file, export_file
 import ovito.modifiers as m#import BondAnalysisModifier, CreateBondsModifier,CoordinationAnalysisModifier,TimeSeriesModifier
-import os
+import glob, os
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
 from scipy.signal import find_peaks
 from matplotlib.animation import FuncAnimation 
+from itertools import cycle
+lines = ["-","--",":","-."]
+colors = ["r","b","g","c","y"]
 
 
-def coordinationTimeseries(file,coordList,timestepLabels=[],title=''):
+def coordinationTimeseries(folderList,coordList,timestepLabels=[],title=''):
+    pipelineList=[]
+    linecycler = cycle(lines)
+    colorcycler = cycle(colors)
+    # os.chdir(folder)
+    for folder in folderList:
+
+        datafile=''
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                if file.endswith(".dump") and file != "bonds.dump":#this uses the folder's dump file
+                    datafile=os.path.join(root,file)
+            
+        try:
+                    
+            pipeline = import_file(datafile)
+            
+            #numframes=pipeline.source.num_frames
+
+            pipeline.modifiers.append(m.CreateBondsModifier(cutoff = 2))
+            pipeline.modifiers.append(m.BondAnalysisModifier(partition=m.BondAnalysisModifier.Partition.ByParticleType,bins = 200))
+            
+            # Export bond angle distribution to an output text file.
+            #export_file(pipeline, 'output/bond_angles.txt', 'txt/table', key='bond-angle-distr', end_frame=1)
+
+            # Convert bond length histogram to a NumPy array and print it to the terminal.
+            data = pipeline.compute()
+            pipelineList.append(pipeline)
+            
+        except Exception as e:
+            print(folder)
+            print(datafile) 
+            print(e)
+        
+    
+
 
     l=len(coordList)
-    # try: 
-    pipeline = import_file(file)
-    
-    numframes=pipeline.source.num_frames
+    fig = plt.figure()
+    for pipeline in pipelineList:
+        curColor = next(colorcycler)
+        #curLine = next(linecycler)
+        numframes=pipeline.source.num_frames
 
-  
-    pipeline.modifiers.append(m.SelectTypeModifier(property = 'Particle Type', types = {'Si'}))
-    pipeline.modifiers.append(m.CoordinationAnalysisModifier(cutoff = 2, number_of_bins = 200,partial=True))
-    pipeline.modifiers.append(m.HistogramModifier(bin_count=200, property='Coordination',only_selected=True))
-    #pipeline.modifiers.append(m.TimeSeriesModifier(operate_on='HistogramModifier.Coordination'))
     
-    
-    ts=np.empty([numframes,l]) 
-    t=np.arange(numframes)
-    for i in t:
+        pipeline.modifiers.append(m.SelectTypeModifier(property = 'Particle Type', types = {'Si'}))
+        pipeline.modifiers.append(m.CoordinationAnalysisModifier(cutoff = 2, number_of_bins = 200,partial=True))
+        pipeline.modifiers.append(m.HistogramModifier(bin_count=200, property='Coordination',only_selected=True))
+        #pipeline.modifiers.append(m.TimeSeriesModifier(operate_on='HistogramModifier.Coordination'))
         
-        data = pipeline.compute(i)
-        # # print(data.objects.Count)
-        # for o in data.objects:
-        #     print(o)
         
-        ea = np.array(data.tables['histogram[Coordination]'].xy())
-        e=ea[:,0]
+        ts=np.empty([numframes,l]) 
+        t=np.arange(numframes)
+        for i in t:
+            
+            data = pipeline.compute(i)
+            # # print(data.objects.Count)
+            # for o in data.objects:
+            #     print(o)
+            
+            ea = np.array(data.tables['histogram[Coordination]'].xy())
+            e=ea[:,0]
+            
+            for n in np.arange(l):
+                ind=np.argmin(np.abs(e-coordList[n]))
+                ts[i,n]=ea[ind][1]
+
         
         for n in np.arange(l):
-            ind=np.argmin(np.abs(e-coordList[n]))
-            ts[i,n]=ea[ind][1]
+            val=ts[:,n]
+            lstr=coordList[n]
+            curLine=lines[n%4]
+            plt.plot(t,val,curLine,color=curColor,label=lstr)
 
-    fig = plt.figure()
-    for n in np.arange(l):
-        val=ts[:,n]
-        lstr=coordList[n]
-        plt.plot(t,val,label=lstr)
-
-    ###### Plot the different labels for time regions
-    xaxislen=fig.gca().get_xlim()[1]
-    figwidth=fig.get_figwidth()
-    figwidth,figheight=fig.canvas.get_width_height()
-    yratio=.15
-    xbuff=15#pixels of buffer from text to vertical line
-    
-    #Ratio of all text's height on figure
-
-    for lbl in timestepLabels:
-        xpos=lbl[0]
-        xratio=xpos/xaxislen
+        ###### Plot the different labels for time regions
+        xaxislen=fig.gca().get_xlim()[1]
+        figwidth=fig.get_figwidth()
+        figwidth,figheight=fig.canvas.get_width_height()
+        yratio=.15
+        xbuff=15#pixels of buffer from text to vertical line
         
-        #print(str(xratio)+'*'+str(figwidth))        
-        plt.axvline(x=xpos)
-        plt.text(x=xpos+xbuff,y=yratio*figheight,s=lbl[1])
+        #Ratio of all text's height on figure
+
+        for lbl in timestepLabels:
+            xpos=lbl[0]
+            xratio=xpos/xaxislen
+            
+            #print(str(xratio)+'*'+str(figwidth))        
+            plt.axvline(x=xpos)
+            plt.text(x=xpos+xbuff,y=yratio*figheight,s=lbl[1])
 
 
-    plt.title(title)
-    plt.legend(loc='upper left',title='Coordination Number')
-    plt.xlabel('Timestep')
-    plt.ylabel('Count')
+        plt.title(title)
+        plt.legend(loc='lower left',title='Coordination Number')
+        plt.xlabel('Timestep')
+        plt.ylabel('Count')
+        
+        # df = pd.DataFrame(e, columns=['word', 'frequency'])
+        # df.plot(kind='bar', x='word')
+        # print(data.tables['histogram[Coordination]'].xy())
+    
+    
     plt.show()
-    # df = pd.DataFrame(e, columns=['word', 'frequency'])
-    # df.plot(kind='bar', x='word')
-    # print(data.tables['histogram[Coordination]'].xy())
-    
-    
-    
     # except:
     #     print(' b ')
     #print(data.tables['time-series'].xy())
@@ -146,12 +186,20 @@ def rdfTimeseries(file,range,out):
         
     
         
-def bondAnalysis(file):
+def bondAnalysis(folder,plot):
     try: 
-        pipeline = import_file(os.path.join(iroot,f))
-        
-        numframes=pipeline.source.num_frames
+        datafile=''
+        # os.chdir(folder)
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                if file.endswith(".data"):#uses a data file not log file
+                    datafile=os.path.join(root,file)
     
+    
+        pipeline = import_file(datafile)
+        
+        #numframes=pipeline.source.num_frames
+
         pipeline.modifiers.append(m.CreateBondsModifier(cutoff = 2))
         pipeline.modifiers.append(m.BondAnalysisModifier(partition=m.BondAnalysisModifier.Partition.ByParticleType,bins = 200))
         
@@ -159,13 +207,12 @@ def bondAnalysis(file):
         #export_file(pipeline, 'output/bond_angles.txt', 'txt/table', key='bond-angle-distr', end_frame=1)
 
         # Convert bond length histogram to a NumPy array and print it to the terminal.
-        data = pipeline.compute(numframes-1)
-    except:
-        i=1
+        data = pipeline.compute()
+    except Exception as e: 
+        print(datafile)
+        print(e)
         
-    # if numframes < 20:
-    #     continue
-    print(d + ' with '+str(numframes)+' frames.')
+
     # if np.isclose(peakLen,realLen,atol=errLen) and np.isclose(peakAngle,realAngle,atol=errAng):
 
 
@@ -175,19 +222,23 @@ def bondAnalysis(file):
     angleTypes=data.tables['bond-angle-distr'].y
     angleBins = angleTables[:,0]
     
+    outlist=[]
+    
     for column, name in enumerate(angleTypes.component_names):
-        if name != 'Si-O-Si' and name != 'O-Si-O':
+        angleCounts=angleTypes[:,column]
+        if np.sum(angleCounts) == 0:
             continue
         
-        angleCounts=angleTypes[:,column]
         
         peakAngle=angleBins[np.argmax(angleCounts)]
-        angTitle= "Angle distribution for bond types:{} with a max angle of {:.1f}".format(name,peakAngle)
-        plt.bar(angleBins,angleCounts)
-        plt.title(angTitle)
-        plt.xlabel('Angle(Degrees)')
-        plt.ylabel('Count')
-        plt.show()
+        angTitle= "Angle distribution for bond types: {} with a max angle of {:.1f}".format(name,peakAngle)
+        outlist.append(angTitle)
+        if plot:
+            plt.bar(angleBins,angleCounts)
+            plt.title(angTitle)
+            plt.xlabel('Angle(Degrees)')
+            plt.ylabel('Count')
+            plt.show()
     
     
     lenTables=data.tables['bond-length-distr'].xy()
@@ -195,20 +246,23 @@ def bondAnalysis(file):
     bondBins=lenTables[:,0]
     
     for column, name in enumerate(bondTypes.component_names):
-        # if name != 'Si-O-Si' and name != 'O-Si-O':
-        #     continue
-        bondCounts=bondTypes[:,column]
 
+        bondCounts=bondTypes[:,column]
+        if np.sum(bondCounts)==0:
+            continue
         peakLen=bondBins[np.argmax(bondCounts)]
-        lenTitle= "Bond length distribution for bond types:{} with peak at {:.2f}".format(name,peakLen)
+        lenTitle= "Bond length distribution for bond types: {} with peak at {:.2f}".format(name,peakLen)
+        outlist.append(lenTitle)
+        if plot:
+            # max_y_lim = max(counts) + 500
+            # print(max_y_lim)
+            # min_y_lim = min(counts)
+            
+            # plt.ylim(min_y_lim, max_y_lim)
+            plt.title(lenTitle)
+            plt.xlabel('Length(r’$\AA$’')
+            plt.bar(bondBins,bondCounts)
+            plt.show()
         
-        
-        # max_y_lim = max(counts) + 500
-        # print(max_y_lim)
-        # min_y_lim = min(counts)
-        
-        # plt.ylim(min_y_lim, max_y_lim)
-        plt.title(lenTitle)
-        plt.xlabel('Length(r’$\AA$’')
-        plt.bar(bondBins,bondCounts)
-        plt.show()
+    for s in outlist:
+        print(s)
